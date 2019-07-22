c952773e955ff8f6c90f13b88f812239a533c290
Abhishek Das <abhishek.das1@lloydsbanking.com>
2019-07-22 20:42:20 +0100

    feat(react-dom): client side rendered

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MyComponent = () => <h1>Abhishek welcomes you</h1>;
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './dist/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './dist/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
