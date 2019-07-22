9671253637dc82e3b2f4c864323848cae390fe3c
Abhishek Das <abhishek.das1@lloydsbanking.com>
2019-07-22 21:29:23 +0100

    Updates

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ var _intro = _interopRequireDefault(require("./components/intro"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var Intro = (0, _intro["default"])();
+
 var App = function App() {
   return _react["default"].createElement("div", {
     className: "jumbotron text-center"
-  }, _react["default"].createElement("h1", null, "Abhishek welcomes you"), _react["default"].createElement("intro", null));
+  }, _react["default"].createElement("h1", null, "Abhishek welcomes you"), _react["default"].createElement(Intro, null));
 };
 
 _reactDom["default"].render(_react["default"].createElement(App, null), document.getElementById('root'));
