1c90127700b8637d7cfd5f2f18862c14be41a2b0
Abhishek Das <abhi.das2007das@gmail.com>
2020-01-26 07:59:21 +0000

    refactor design

--- src/resumeConstant.js
+++ src/resumeConstant.js
@@ -22,4 +22,9 @@ export const languagesArr = [
   ['English', 'proficient']
 ];
 
-export const workExperiencesArr = [];
+export const workExperiencesArr = [
+  ['DevOps Engineer', 'May 2019 - Present', 'Lloyds Banking Group', 'London, GB'],
+  ['Full-stack Developer', 'May 2017 - Apr 2019', 'Lloyds Banking Group', 'London, GB'],
+  ['Technical Consultant | Test Automation Engineer', 'Feb 2016 - Mar 2017', 'Royal Dutch Shell', 'London, GB'],
+  ['Software Engineer', 'Jan 2012 - Jan 2016', 'Wipro Technologies', 'India'],
+];
